992b13cda5e95dec58bf84b2424e232f692cdaf7
ol-iva <lena.ivakh@gmail.com>
2022-11-12 12:52:06 +0100

    Add return null to forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -88,6 +88,7 @@ export default function Forecast(props) {
                            </div>
                        );
                     }
+                    return null;
                 })
                 }
 
